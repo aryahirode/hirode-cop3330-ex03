f10b98e2067f5f798fbb3317c8f2270b4bf5337d
Arya Hirode <aryahirode2001@gmail.com>
2021-06-03 19:05:26 -0400

    Added quotePrompt method

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -1,5 +1,16 @@
 package base;
 
+import java.util.Scanner;
+
 public class App {
-    
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        App myApp = new App();
+    }
+
+    private String quotePrompt() {
+        System.out.print("What is the quote? ");
+        return in.nextLine();
+    }
+
 }
